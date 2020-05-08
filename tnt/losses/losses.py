7269004c92cec021ce35ec4c345191b1ae00aa35
BigFishMaster <yancanxiang@163.com>
2020-05-08 13:17:06 +0800

    fix bug of RelativeLabelLoss and its V2 version

--- tnt/losses/losses.py
+++ tnt/losses/losses.py
@@ -14,33 +14,36 @@ class RelativeLabelLossV2(torch.nn.Module):
     def forward(self, x, y):
         # loss1
         y = y.long()
-        target = y[:, 0]
-        loss1 = self.ce(x, target)
+        y_mask = (y != -1).view(-1)
+        _, num_labels = y.shape
+        t, r = torch.split(y, [1, num_labels-1], dim=1)
+        loss1 = self.ce(x, t.view(-1))
         # loss2
         batch_size, class_dim = x.shape
-        pos_mask = y != -1
-        sample_mask = pos_mask.float().sum(1) > 1
+        pos_mask = r != -1
+        sample_mask = pos_mask.float().sum(1) > 0
         # positive predictions
-        neg_mask = y == -1
-        y[neg_mask] = 0
-        pos_data = torch.gather(x, dim=1, index=y)
-        pos_data[neg_mask] = 1e8
-        min_relative_index = pos_data.argmin(1, keepdim=True)
-        min_relative_data = torch.gather(pos_data, dim=1, index=min_relative_index)
+        neg_mask = r == -1
+        r[neg_mask] = 0
+        pos_data = torch.gather(x, dim=1, index=r)
+        mask_pos_data = torch.where(neg_mask, torch.tensor(1e8, device=x.device), pos_data)
+        min_relative_index = mask_pos_data.argmin(1, keepdim=True)
+        min_relative_data = torch.gather(mask_pos_data, dim=1, index=min_relative_index)
         # negative predictions
-        pos_mask = pos_mask.view(-1)
         bias = torch.arange(batch_size, dtype=torch.long, device=x.device) * class_dim
         new_index = bias.view(batch_size, 1) + y
         new_index = new_index.view(-1)
         # available positive mask
-        new_index = new_index[pos_mask]
-        new_data = x.contiguous().view(-1)
-        new_data[new_index] = -1e8
-        new_data = new_data.view(batch_size, class_dim)
+        new_index = new_index[y_mask]
+        flag = torch.ones(batch_size, class_dim, device=x.device).bool().view(-1)
+        flag[new_index] = False
+        flag = flag.view(batch_size, class_dim)
+        new_data = torch.where(flag, x, torch.tensor(-1e8, device=x.device))
+        #new_data = new_data.view(batch_size, class_dim)
         relative_data = torch.cat([min_relative_data, new_data], dim=1)
-        relative_data = relative_data[sample_mask]
-        target_label = torch.zeros(relative_data.size(0), dtype=torch.long, device=relative_data.device)
-        loss2 = F.cross_entropy(relative_data, target_label)
+        cand_data = relative_data[sample_mask]
+        target_label = torch.zeros(cand_data.size(0), dtype=torch.long, device=x.device)
+        loss2 = F.cross_entropy(cand_data, target_label)
 
         loss = loss1 + self.gamma * loss2
         self.loss1 = loss1.item()
@@ -85,7 +88,9 @@ class RelativeLabelLoss(torch.nn.Module):
             ones = torch.ones(class_dim, device=index.device)
             # mask the negative indices
             ones[index_selected] = 0
-            # gather data from target label and its relative labels.
+            # skip the first target label.
+            index_selected = index_selected[1:]
+            # gather data from relative labels.
             pred = data.gather(0, index_selected)
             min_pred_index = pred.argmin()
             relative_index = index_selected[min_pred_index]
@@ -161,15 +166,73 @@ class WeightLabelLoss(torch.nn.Module):
         return w_l
 
 
-def test_relativelabelloss():
+def test_relativelabelloss_v2_check_inplace_operation():
+    torch.manual_seed(123)
+    torch.set_printoptions(6)
+
+    with torch.autograd.set_detect_anomaly(True):
+        ce = torch.nn.CrossEntropyLoss()
+        gamma = 0.2
+        x = torch.rand(4, 10, requires_grad=True)
+        y = torch.Tensor([[1,2,0,-1,-1], [1,-1,-1,-1,-1],[2,-1,-1,-1,-1],[3,2,1,0,4]])
+        # loss1
+        y = y.long()
+        y_mask = (y != -1).view(-1)
+        _, num_labels = y.shape
+        t, r = torch.split(y, [1, num_labels-1], dim=1)
+        loss1 = ce(x, t.view(-1))
+        # loss2
+        batch_size, class_dim = x.shape
+        pos_mask = r != -1
+        sample_mask = pos_mask.float().sum(1) > 0
+        # positive predictions
+        neg_mask = r == -1
+        r[neg_mask] = 0
+        pos_data = torch.gather(x, dim=1, index=r)
+        mask_pos_data = torch.where(neg_mask, torch.tensor(1e8, device=x.device), pos_data)
+        min_relative_index = mask_pos_data.argmin(1, keepdim=True)
+        min_relative_data = torch.gather(mask_pos_data, dim=1, index=min_relative_index)
+        # negative predictions
+        bias = torch.arange(batch_size, dtype=torch.long, device=x.device) * class_dim
+        new_index = bias.view(batch_size, 1) + y
+        new_index = new_index.view(-1)
+        # available positive mask
+        new_index = new_index[y_mask]
+        flag = torch.ones(batch_size, class_dim, device=x.device).bool().view(-1)
+        flag[new_index] = False
+        flag = flag.view(batch_size, class_dim)
+        new_data = torch.where(flag, x, torch.tensor(-1e8, device=x.device))
+        #new_data = new_data.view(batch_size, class_dim)
+        relative_data = torch.cat([min_relative_data, new_data], dim=1)
+        cand_data = relative_data[sample_mask]
+        target_label = torch.zeros(cand_data.size(0), dtype=torch.long, device=x.device)
+        loss2 = F.cross_entropy(cand_data, target_label)
+
+        loss = loss1 + gamma * loss2
+
+
+def test_relativelabelloss_v2():
     loss_fn = RelativeLabelLossV2()
     torch.manual_seed(123)
-    x = torch.rand(4, 10)
+    torch.set_printoptions(6)
+    x = torch.rand(4, 10, requires_grad=True)
     y = torch.Tensor([[1,2,0,-1,-1], [1,-1,-1,-1,-1],[2,-1,-1,-1,-1],[3,2,1,0,4]])
     import time
     start = time.time()
-    for i in range(100):
-        loss = loss_fn(x, y)
+    loss = loss_fn(x, y)
+    print("time:", time.time() - start)
+    print(loss)
+
+
+def test_relativelabelloss():
+    loss_fn = RelativeLabelLoss()
+    torch.manual_seed(123)
+    torch.set_printoptions(6)
+    x = torch.rand(4, 10, requires_grad=True)
+    y = torch.Tensor([[1,2,0,-1,-1], [1,-1,-1,-1,-1],[2,-1,-1,-1,-1],[3,2,1,0,4]])
+    import time
+    start = time.time()
+    loss = loss_fn(x, y)
     print("time:", time.time() - start)
     print(loss)
 
@@ -194,3 +257,4 @@ if __name__ == "__main__":
     #test_multilabelloss()
     #test_weightlabelloss()
     test_relativelabelloss()
+    test_relativelabelloss_v2()
