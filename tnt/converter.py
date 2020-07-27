231f9b7d8d397cee6b98c5b9c1c16b4ae675a28b
BigFishMaster <yancanxiang@163.com>
2020-07-27 10:53:06 +0800

    update

--- tnt/converter.py
+++ tnt/converter.py
@@ -52,17 +52,13 @@ class CustomModule(tf.Module):
         self.preserve_aspect_ratio = preserve_aspect_ratio
         self.mean = mean
         self.std = std
-        print("mean:", mean, "std:", std)
-        print("Initialize ok.")
 
     @tf.function(input_signature=[tf.TensorSpec([None, None, 3], tf.uint8, name="input_image")])
-    def __call__(self, image):
+    def extract_feature(self, image):
         #TODO: check the order of h and w.
         h = tf.shape(image)[0]
         w = tf.shape(image)[1]
-        #tf.print("image size:", h, w)
         image_data = tf.cast(image, tf.float32)
-        #tf.print("image_data:", image_data, image_data.dtype)
 
         def resize_and_extract(size, target):
             if self.preserve_aspect_ratio:
@@ -77,38 +73,25 @@ class CustomModule(tf.Module):
                 else:
                     oh = size
                     ow = tf.cast(size * w / h, tf.int32)
-                    #tf.print("oh, ow:", oh, ow)
                     scaled_image = tf.image.resize(image_data, [oh, ow], antialias=True)
                     scaled_image = tf.round(scaled_image)
-                    #tf.print("In h<w, scaled_image:", scaled_image.shape)
                     resized_image = tf.image.resize_with_crop_or_pad(scaled_image, target, target)
-                    #tf.print("In h<w, resized_image:", resized_image.shape)
             else:
                 scaled_image = tf.image.resize(image_data, [size, size], antialias=True)
                 scaled_image = tf.round(scaled_image)
-                #tf.print("scaled_image:", scaled_image, scaled_image.shape)
                 resized_image = tf.image.resize_with_crop_or_pad(scaled_image, target, target)
-                #tf.print("resized_image:", resized_image, resized_image.shape)
             preprocess = resized_image / self.scale
-            #tf.print("preprocess scaled:", preprocess)
-            #tf.print("normalize mean:", self.mean, self.mean.shape)
-            #tf.print("normalize std:", self.std, self.std.shape)
             preprocess = (preprocess - self.mean) / self.std
-            #tf.print("preprocess normalized:", preprocess)
             # shape: 3 x 224 x 224
             preprocess = tf.transpose(preprocess, perm=[2, 0, 1])
-            #tf.print("preprocess transposed:", preprocess.shape)
             # shape: 1 x 3 x 224 x 224
             preprocess = tf.expand_dims(preprocess, axis=0)
-            #tf.print("preprocess expanded:", preprocess.shape)
-            #tf.print("preprocess input:", preprocess)
             # shape: 1 x 2048
             output = self.model_func(preprocess)
             return output
 
         output_global = resize_and_extract(self.size[0], self.target[0])
         num_size = tf.shape(self.size)[0]
-        #tf.print("num_size:", num_size)
         for ind in tf.range(1, num_size):
             tf.autograph.experimental.set_loop_options(
                 shape_invariants=[(output_global, tf.TensorShape([None, None]))])
@@ -240,14 +223,14 @@ def convert(config):
     target = tf.constant(image_target_list, dtype=tf.int32, name="image_target_list")
     size = tf.constant(image_size_list, dtype=tf.int32, name="image_size_list")
     mean = tf.constant(model.mean, dtype=tf.float32, shape=(1,1,3), name="input_mean")
-    std = tf.constant(model.std, dtype=tf.float32, shape=(1,1,3), name="input_mean")
+    std = tf.constant(model.std, dtype=tf.float32, shape=(1,1,3), name="input_std")
     print("tf mean:", mean, "tf std:", std)
     module = CustomModule(output_tfgraph_name, l2norm=l2norm, scale=scale,
                           preserve_aspect_ratio=preserve_aspect_ratio,
                           target=target, size=size, mean=mean, std=std)
     output_savedmodel_name = os.path.join(output_dir, "saved_model/")
     signatures = {
-        'serving_default': module.__call__.get_concrete_function(),
+        'serving_default': module.extract_feature,
     }
     tf.saved_model.save(module, output_savedmodel_name, signatures=signatures)
     logger.info("saving tf-2.2 model to {} is ok.".format(output_savedmodel_name))
