3f70415797071e85dc60f829befe359951b62691
BigFishMaster <yancanxiang@163.com>
2020-08-15 01:22:32 +0800

    update

--- tnt/pretrainedmodels/models/resnet_wider/__init__.py
+++ tnt/pretrainedmodels/models/resnet_wider/__init__.py
@@ -2,3 +2,5 @@ from .resnet_wider import resnet101x1
 from .resnet_wider import resnet101x1_sk
 from .resnet_wider import resnet152x1
 from .resnet_wider import resnet152x1_sk
+from .resnet_wider import resnet101x2
+from .resnet_wider import resnet101x2_sk
